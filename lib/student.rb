edde62d0fddeac469a875c537f04f2fbcf6aca20
John Hayes <johnnyhayes7@gmail.com>
2019-03-26 12:13:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,57 @@
+require "pry"
 class Student
   attr_accessor :id, :name, :grade
-
+  
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+    
+    DB[:conn].execute(sql)
   end
-
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.students_below_12th_grade
+    below_12th = []
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade < 12
+    SQL
+    
+    row = DB[:conn].execute(sql)
+    binding.pry
+  end
+  
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
